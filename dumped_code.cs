99777baa6443d6dd9906226ae212fe8dbfeeeeaa
Kyle Pham <44920500+kylesmile1103@users.noreply.github.com>
2020-09-08 09:42:00 +0700

    upload dumped code

---
+++ dumped_code.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Generated code file by Il2CppInspector - http://www.djkaty.com - https://github.com/djkaty
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Image 22: Assembly-CSharp.dll - Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null - Types 2194-2195
+[assembly: CompilationRelaxations] // 0x002327D8-0x002328A4
+[assembly: Debuggable] // 0x002327D8-0x002328A4
+[assembly: RuntimeCompatibility] // 0x002327D8-0x002328A4
+
+public class FieldTest : MonoBehaviour // TypeDefIndex: 2195
+{
+	// Fields
+	private int a; // 0x18
+	private bool b; // 0x1C
+	private string str; // 0x20
+	private float c; // 0x28
+	private int[] ar; // 0x30
+	private List<int> listInt; // 0x38
+	private List<string> listStr; // 0x40
+	public UnityEngine.UI.Text txtShowResult; // 0x48
+	public static int staticA; // 0x00
+	public static string staticStr; // 0x08
+
+	// Constructors
+	public FieldTest(); // 0x0066941C-0x0066941C
+
+	// Methods
+	private void Start(); // 0x00667D80-0x00667F14
+	public void ShowResult(); // 0x00667F14-0x006684E8
+	public void ChangeA(); // 0x006684E8-0x00668518
+	public void changeB(); // 0x00668518-0x00668558
+	public void ChangeStr(); // 0x00668558-0x006685EC
+	public void ChangeC(); // 0x006685EC-0x00668620
+	public void ChangeAr(); // 0x00668620-0x0066869C
+	public void changeListInt(); // 0x0066869C-0x00668738
+	public void ChaneListStr(); // 0x00668738-0x00668834
+	public int GetIntA(int val); // 0x00668834-0x006688E8
+	public bool GetBoolB(bool val); // 0x006688E8-0x0066898C
+	public string GetStringStr(string val); // 0x0066898C-0x00668A1C
+	public float GetFloatC(float val); // 0x00668A1C-0x00668AD8
+	public int[] GetArrayInt(int[] val); // 0x00668AD8-0x00668BA0
+	public void ChangeValueStaticIntA(); // 0x00668BA0-0x00668C34
+	public void changeValueStringStatic(); // 0x00668C34-0x00668D00
+	public List<int> GetListInt(List<int> val); // 0x00668D00-0x00668D78
+	public List<string> GetListStr(List<string> val); // 0x00668D78-0x00668DF0
+	public void ShowResultMethod(); // 0x00668DF0-0x0066941C
+}
+
